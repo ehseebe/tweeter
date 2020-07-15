cfcbe7c65253d36089b05a8aa2a3d25ee0ce346d
Alyssa Beaton <ehseebe@gmail.com>
2020-07-15 20:56:22 +0000

    Removed unecessary logs

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,13 +1,6 @@
 $(document).ready(function() {
 
-  // selector => #tweet-text is my form textarea
-  // section => new-tweet
-  // button = anonymous (in tweet-form-footer)
-  // counter = counter (in tweet-form-footer)
-  // event is change or blur
-  // use .this, make sure it's capturing the right thing
   const $tweetText = ('#tweet-text');
-  
 
   $($tweetText).on('keyup', function(event) {
     const charLength = $(this).val().length;
@@ -25,10 +18,4 @@ $(document).ready(function() {
   
   })
 
-
-
-
-
-  // need to append tweet to initial-tweets.json
-  // 
 });
