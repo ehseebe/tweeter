8e973ca4700c794911d91722baada33c9bb47320
Alyssa Beaton <ehseebe@gmail.com>
2020-07-15 18:37:30 +0000

    Implemented renderTweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,19 +4,42 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
+const tweetData = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
       "handle": "@SirIsaac"
     },
-  "content": {
+    "content": {
       "text": "If I have seen further it is by standing on the shoulders of giants"
     },
-  "created_at": 1461116232227
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+];
+
+//loops through tweets, applies form, outputs to page
+const renderTweets = (tweetData) => {
+  for(let item of tweetData) {
+    $('#tweet-container').append(createTweetElement(item));
+  } 
 }
 
-const createTweetElement = (tweetData) => {
+//tweet form
+const createTweetElement = (tweet) => {
+  let readableDate = new Date(tweet.created_at);
+  
   const $tweet =  `
   <article class='article-tweet'>
 
@@ -24,26 +47,26 @@ const createTweetElement = (tweetData) => {
 
         <span class='tweet-profile'>
           <span class='tweet-profile-pic'>
-            <img src="${tweetData.avatars}" alt="profile picture" width="35" height="35">
+            <img src="${tweet.user.avatars}" alt="profile picture" width="35" height="35">
               </span>
               <span class='tweet-profile-name'>
-                  ${tweetData.name}
+                  ${tweet.user.name}
             </span>
         </span> 
 
               <span class='tweet-handle'>
-              ${tweetData.handle}
+              ${tweet.user.handle}
             </span>
       </header>
 
         <div class="tweet-content">
-          <p>${tweetData.content.text}</p>
+          <p>${tweet.content.text}</p>
         </div>
 
         <footer class='article-tweet-footer'>
 
           <span class='tweet-date'>
-            ${tweetData.created_at} days ago
+            ${readableDate} days ago
             </span>
 
           <span class='tweet-icons'>
@@ -60,43 +83,6 @@ const createTweetElement = (tweetData) => {
 }
 
 
-const renderTweets = (tweet) => {
-  const db = 'http://localhost8080/tweets';
-  //GET INFORMATION from an API ( a server with data )
-  $.ajax({
-      url: apiWithParam,
-      method: "GET",
-  }).then( (response) => {
-      $('#item-container').empty()
-      console.log("AJAX call came back with response");
-      response.forEach( (element) => {
-          if ( element ) {
-
-              console.log(element.show.image);
-              let tempItem = createItem(element);
-              $('#item-container').append(tempItem);
-          }
-      })
-  }).catch((e) => console.log(e));
-  //then once we got the information, we will use the function we built (createItem)
-  // to append multiple items we found
-
-}
-
-const $tweet = createTweetElement(tweet);
-
-// Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
-$('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
-
 $(document).ready(function() {
-  // $('#search').on('submit', (evt) => {
-  //     evt.preventDefault();
-  //     //console.log(evt.target.searchParam.value);
-  //     const searchParameter = evt.target.searchParam.value;
-  //     searchItem(searchParameter);
-  // })
-
-  console.log("READY!!");
-
+  renderTweets(tweetData);
 })
